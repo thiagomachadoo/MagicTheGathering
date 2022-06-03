60f0f57d628453e1e8155930cabb36d52a5ca4f6
Thiago Machado <56802688+thiagomachadoo@users.noreply.github.com>
2022-06-02 22:44:41 -0300

    ajustando documentação swagger para um melhor entendimento da api

--- src/main/java/com/projectcard/card/entity/CardsDTO.java
+++ src/main/java/com/projectcard/card/entity/CardsDTO.java
@@ -2,6 +2,9 @@ package com.projectcard.card.entity;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +20,9 @@ import javax.persistence.Id;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(name = "CardsDTO")
 @Entity(name = "GAME_OF_CARDS")
 public class CardsDTO {
 
@@ -24,16 +30,22 @@ public class CardsDTO {
     @GeneratedValue
     private Integer id;
 
+    @Schema(description = "Nome da carta", example = "Raichu GX", required = true)
     private String nameOfCard;
 
+    @Schema(description = "Edição da carta", allowableValues = {"BASIC","PLATINUM","PREMIUM","PREMIUM PLUS"}, example = "PLATINUM", required = true)
     private String edition;
 
+    @Schema(description = "Idioma da carta", allowableValues = "pt-br", required = true)
     private String language;
 
+    @Schema(description = "É uma carta laminada", example = "true", required = true)
     private Boolean foil;
 
+    @Schema(description = "Quantas cartas você tem", example = "426", required = true)
     private int theAmount;
 
+    @Schema(description = "Quanto vale em R$ sua carta", type = "double",example = "54.700", required = true)
     private double price;
 
     public CardsDTO(Object o) {
